1cd7f7cc63b12826782cad65deee9b31002c3313
Neil Houselander <neilhouselander@sky.com>
2017-08-09 09:40:10 +0100

    half way there labels added but not working yet

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -11,19 +11,28 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    //create the sprite variables
     var ball = SKSpriteNode()
     var main = SKSpriteNode()
     var enemy = SKSpriteNode()
     
+    //score array - use rather than 2 separate variables
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
         
+        //run start game to set scores to zero
+        startGame()
+        
+        //link variables with spritenodes
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         main = self.childNode(withName: "main") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
         
-        
+        //set initial velocity and direction for the ball node
         ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
         
+        //create a frame which follows the frame of the screen so we can apply physics to it
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
 
         border.friction = 0
@@ -33,6 +42,38 @@ class GameScene: SKScene {
         
     }
     
+    func startGame() {
+        
+        score = [0,0]
+        
+        
+    }
+    
+    //function to add scores and reset ball to centre when a point is scored, set velocity back to zero, then start again
+    func addScore(playerWhoWon: SKSpriteNode) {
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main {
+            
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+            
+        }
+        else if playerWhoWon == enemy {
+            
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+            
+        }
+        
+        print(score)
+        
+        
+    }
+    
+    //use touches to move my paddle - get location of touch and then move main paddle to that x location
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
@@ -42,8 +83,7 @@ class GameScene: SKScene {
             main.run(mainMoveAction)
             
         }
-        
-        
+
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -55,24 +95,30 @@ class GameScene: SKScene {
             main.run(mainMoveAction)
             
         }
-        
-        
+
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         
-        let enemyMoveAction = SKAction.moveTo(x: ball.position.x, duration: 0.3)
+        //every time the game updates (60 times per second) run an action for the enemy paddle to track the ball
+        let enemyMoveAction = SKAction.moveTo(x: ball.position.x, duration: 0.4)
         enemy.run(enemyMoveAction)
         
-        
-        
-        
+        //if the balls y position is less than main then enemy scores a point - run addScore function & pass in enemy parameter
+        if ball.position.y <= main.position.y - 70 {
+            
+            addScore(playerWhoWon: enemy)
+            
+        }
+        else if ball.position.y >= enemy.position.y + 70 {
+            
+            addScore(playerWhoWon: main)
+            
+        }
+
         
     }
-    
-    
-    
     
     
 }
